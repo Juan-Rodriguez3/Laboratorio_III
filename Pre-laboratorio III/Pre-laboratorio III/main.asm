22b7c1a6b3cc5eeec64f55194de98bd466e8206c
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-02-21 01:28:16 -0600

    Primera prueba
    Primer boceto de código

--- Pre-laboratorio III/Pre-laboratorio III/main.asm
+++ Pre-laboratorio III/Pre-laboratorio III/main.asm
@@ -1,5 +1,4 @@
-;*********************
-; Universidad del Valle de Guatemala
+	; Universidad del Valle de Guatemala
 ; IE2025: Programacion de Microcontroladores
 ;
 ; Author: Juan Rodriguez
@@ -7,15 +6,15 @@
 ; Hardware: ATmega328P
 ; Creado: 20/02/2025
 ; Modificado: 20/02/2025
-; Descripcion: Implementación de interrupciones
+; Descripcion: Implementaci?n de interrupciones
 ;*********************
 
 .include "M328PDEF.inc"
 .cseg
 .org 0x0000
-    RJMP SETUP  ; Ir a la configuración al inicio
+    RJMP SETUP  ; Ir a la configuraci?n al inicio
 
-.org PCI1addr  ; Vector de interrupción para PCINT1 (PORTC)
+.org PCI1addr  ; Vector de interrupci?n para PCINT1 (PORTC)
     RJMP ISR_PCINT1
 
 	// Configuracion de la pila
@@ -43,7 +42,7 @@ SETUP:
 	LDI		R16, (PCIE1<<1)					//Encender PCIE1 en PCICR
 	STS		PCICR, R16						//Configurar interrupciones puerto C
 	LDI		R16, (PCINT8<<1) | (PCINT9<<1)	//Configurar PC0 y PC1 en 
-	STS		PCMSK1, R16						//Cargar la configuración de los pines
+	STS		PCMSK1, R16	
 
 	NOP
 	NOP
@@ -56,18 +55,37 @@ SETUP:
 MAIN:
 	RJMP	MAIN
 
-//Subrutinas de interrupción
+//Subrutinas
 
-ISR_PCINT1:
-	PUSH	R16
-	IN		R16, SREG						//Guardar en el stack el sreg
-	PUSH	R16
-
-	IN		R17, PINC						//Leer 
+SUMA:
+	INC		R18								//Incrementa contador
+	CPI		R18, 0x10						//Comparar con 16
+	BREQ	OVERF
 	
-	POP		R16
-	OUT		SREG, R16
-	POP		R16
+	RJMP	FIN
+RESTA:
+	CPI		R18, 0x00						//Compara el contador			
+	BREQ	UNDERF							
+	DEC		R18								//Decrementa el contador
+	RJMP	FIN
 
+OVERF:
+	LDI		R18, 0x00						//Reiniciar el contador a 0
+	RJMP	FIN
+UNDERF:
+	LDI		R18, 0x0F						//Reiniciar el contador a 15
+	RJMP	FIN
+
+
+//Subrutinas de interrupciön
+ISR_PCINT1:	
+	IN		R17, PINC						//Leer el estado de los botones
+	SBRS	R17, 0							//Salta si el bit 0 esta en set
+	RJMP	SUMA
+	SBRS	R17, 1							//Salta si el bit 1 esta en reset
+	RJMP	RESTA
+FIN:
+	OUT		PORTB,	R18						//Actualiza la salida,
+	RETI
 
 
