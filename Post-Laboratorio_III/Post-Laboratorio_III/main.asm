4cd6c5f614f5a309e65c4e1c4092f824c6c11d95
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-02-25 19:22:28 -0600

    Implementación de Timer2
    Creamos un delay para ser más precisos a la hora de multiplexear

--- Post-Laboratorio_III/Post-Laboratorio_III/main.asm
+++ Post-Laboratorio_III/Post-Laboratorio_III/main.asm
@@ -13,8 +13,8 @@
 .def	DISPLAY=R18
 .def	CONTADOR=R19
 .def	UNI_DISP=R20
-.def	DEC_DISP=R22
-.def	BOTON=R21
+.def	DEC_DISP=R21
+
 
 
 
@@ -54,7 +54,7 @@ SETUP:
 	//Puerto C como entrada y Pull-up activados
 	LDI		R16, 0b00001100					//Pin 0 y 1 entradas - Pin 2 y 3 Salidas
 	OUT		DDRC, R16						
-	LDI		R16, 0x00000011					//Pullup en PC0, PC1 - 0 lógico PC2, PC3
+	LDI		R16, 0b11110011					//Pullup en PC0, PC1 - 0 lógico PC2, PC3
 	OUT		PORTC, R16
 
 
@@ -69,6 +69,7 @@ SETUP:
 	//Valor inicial de variables generales
 	LDI		DISPLAY, 0x00
 	LDI		CONTADOR, 0x00
+	LDI		R16, 0x00
 
 	//Usar el puntero Z como salida de display de unidades
 	LDI		ZH, HIGH(TABLA<<1)				//Carga la parte alta de la dirección de tabla en el registro ZH
@@ -81,6 +82,13 @@ SETUP:
 	LDI		XH, HIGH(TABLA<<1)				//Carga la parte alta de la dirección de tabla en el registro ZH
 	LDI		XL, LOW(TABLA<<1)				//Carga la parte baja de la dirección de la tabla en el registro ZL
 
+	DELAY_TIMER2:
+    LDI     R16, (1 << WGM21)        ; Configurar Timer2 en modo CTC (WGM21=1, WGM20=0)
+    STS     TCCR2A, R16
+    LDI     R16, (1 << CS22)         ; Prescaler 16 para 1 MHz
+    STS     TCCR2B, R16
+	LDI     R16, 195					; 256 = 195 (esto es lo que entra en OCR2A)
+	STS     OCR2A, R16	
 
 	SEI										//Habilitar las interrupciones globales.
 
@@ -93,7 +101,7 @@ MAIN:
 	OUT		PORTD, DISPLAY					//Cargar en los display
 	SBI		PORTC, 3						//Encender el display de unidades
 	CBI		PORTC, 2						//Apagar el display de decenas
-	CALL	DELAY
+	//CALL	DELAY
 	//Cargar las decenas
 	MOV		R24, ZL
 	MOV		R25, ZH
@@ -118,12 +126,19 @@ INIT_TMR0:
 	OUT		TCNT0, R16						// Cargar valor inicial en TCNT0
 	RET
 
-//Rutina de interrupción TIMER
+//*******Rutina de interrupción TIMER********
 ISR_TIMER0:
+	PUSH	R16
+	IN		R16, SREG  ; Carga el valor de SREG en R16
+	PUSH	R16
+	SBI     TIFR0, TOV0						; Limpiar bandera de interrupción del Timer0 Overflow
 	INC		CONTADOR
 	CPI		CONTADOR, 100					//Cada interrupción ocurre 10 ms*100=1000ms
 	BREQ	INCREMENTAR
 FIN0:
+	POP		R16
+	OUT		SREG, R16
+	POP		R16
 	RETI
 //
 INCREMENTAR:
@@ -149,15 +164,14 @@ OVERFD:
 	LDI		XL, LOW(TABLA<<1)				//Carga la parte baja de la dirección de la tabla en el registro ZL
 	LDI		DEC_DISP, 0x00					//Reseteamos decenas
 	RJMP	FIN0		
+//*******Rutina de interrupción TIMER********
 
 
 DELAY:
-	LDI R23, 10
-LOOP_DELAY:
-	DEC R23
-	CPI R23, 0
-	BRNE LOOP_DELAY
-	RET
+    SBIS    TIFR2, OCF2A        ; Esperar hasta que OCF2A se active
+    RJMP    DELAY
+    SBI     TIFR2, OCF2A        ; Limpiar la bandera escribiendo 1
+    RET
 
 // Tabla de conversión hexadecimal a 7 segmentos
 TABLA:
